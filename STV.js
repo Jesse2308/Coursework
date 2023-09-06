c21df3a309b85118a96621473fc5d65ed08595fb
Jesse2308 <jrnungaray@hotmail.com>
2023-09-06 16:55:57 -0700

    First commit

---
+++ STV.js
@@ -0,0 +1,28 @@
+/*Create html file to connect to java script */
+/**Create 6 constant to make a formaul for my STV alert pop up */
+/** Have a constant equal dash to seprate numbers**/
+/**Next have three constant equal an equastion */
+/**Have those three equastions equal three sets of numbers */
+/**Now we input '.alert' to get our code to pop up on web page */
+/**Once alert is working we write our statement to add with said codes */
+/**Now that we have statment wrtttien out we add our three constant 'add1,add2,add3'at the end of written statement */
+/**Make sure we add our constant '-' to space numbers*/
+/**Then we test alert on web page */
+
+
+
+
+
+const lock1 = 1;
+const lock2 = 10;
+const lock3 = 90;
+const lock4 = 50;           /** These 6 constant are created to help make a solution for a code */
+const lock5 = 12;
+const lock6 = 27;
+const space = '-';
+
+const add1 = lock1 * lock2;
+const add2 = lock3 - lock4;  /** 3 constant to make a problem equal a solution */
+const add3 = lock5 + lock6;
+
+window.alert('You have received this message because you have been chosen to open an important vault. Here is the secret combination:'+ add1 +space+ add2 + space+ add3);
